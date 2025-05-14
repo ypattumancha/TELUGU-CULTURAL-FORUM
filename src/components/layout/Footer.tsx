6bc97d5afb81f3e38546965e6ed3fdb36c5a13ea
ypattumancha <135755316+ypattumancha@users.noreply.github.com>
2025-05-14 16:38:03 +0000

    color change

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
   
   return (
-    <footer className="bg-primary-900 text-white pt-12 pb-6">
+    <footer className="bg-black text-white pt-12 pb-6">
       <div className="container-custom">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mb-8">
           {/* About Section */}
